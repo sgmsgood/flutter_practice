f1e77b1d1648909421d14fbbe10fbc7ce7843a5f
jisu <45481968+sgmsgood@users.noreply.github.com>
2023-12-04 23:22:34 +0900

    customInputField

--- flutter_order_app/README.md
+++ flutter_order_app/README.md
@@ -1,16 +1,10 @@
 # flutter_order_app
 
-A new Flutter project.
+- Inflearn 강의를 들으며 flutter 개념 다지는 프로젝트
 
-## Getting Started
+### 1. Authentication
 
-This project is a starting point for a Flutter application.
+### 2. Pagenation
 
-A few resources to get you started if this is your first Flutter project:
+### 3. Data modeling
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
---
+++ flutter_order_app/lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_order_app/common/const/colors.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autoFocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField(
+      {required this.onChanged, this.obscureText = false, this.autoFocus = false, this.hintText, this.errorText, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(color: INPUT_BORDER_COLOR, width: 1.0),
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      obscureText: obscureText,
+      autofocus: autoFocus,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: hintText,
+        // errorText: errorText,
+        hintStyle: const TextStyle(color: BODY_TEXT_COLOR, fontSize: 14),
+        fillColor: INPUT_BG_COLOR,
+        filled: true,
+        border: baseBorder,
+        enabledBorder: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR
+          ),
+        ),
+      ),
+    );
+  }
+}
